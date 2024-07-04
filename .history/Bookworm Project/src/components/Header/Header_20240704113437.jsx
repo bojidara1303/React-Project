2cc9672a3f9b646b92b132cee4c7d16e59dd653c
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-07-04 11:39:22 +0300

    Adding stylization to the navigation

---
+++ .history/Bookworm Project/src/components/Header/Header_20240704113437.jsx
@@ -0,0 +1,25 @@
+import { Link, NavLink } from 'react-router-dom'
+import styles from './Header.module.css'
+
+export default function Header() {
+    return (
+        <header>
+            <nav className={styles["site-nav"]}>
+                <img src="/images/Logo-Bookworm.png" alt="logo" />
+                <ul>
+                    {/* <li><Link style={{ textDecoration: 'none', color: 'white' }} to="/">Home</Link></li>
+                    <li><Link style={{ textDecoration: 'none', color: 'white' }} to="/catalog">Catalog</Link></li>
+                    <li><Link style={{ textDecoration: 'none', color: 'white' }} to="/add">Add Book</Link></li>
+                    <li><Link style={{ textDecoration: 'none', color: 'white' }} to="/login">Login</Link></li>
+                    <li><Link style={{ textDecoration: 'none', color: 'white' }} to="/register">Register</Link></li> */}
+
+                    <li><NavLink className={styles["nav-link "]} to="/">Home</NavLink></li>
+                    <li><NavLink className={styles["nav-link "]} to="/catalog">Catalog</NavLink></li>
+                    <li><NavLink className={styles["nav-link "]} to="/add">Add Book</NavLink></li>
+                    <li><NavLink className={styles["nav-link "]} to="/login">Login</NavLink></li>
+                    <li><NavLink className={styles["nav-link "]} to="/register">Register</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
