a142ee79318c30c2e68b5897a72094a07ce82021
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-07-09 14:55:05 +0300

    Adding stylization to the All Books component

--- Bookworm Project/.history/client/src/components/Book/Book_20240709144144.jsx
+++ Bookworm Project/.history/client/src/components/Book/Book_20240709144144.jsx
@@ -7,11 +7,13 @@ export default function Book({
     _id
 }) {
     return (
+        // <div className={styles["book-container"]}>
             <article className={styles["book"]}>
                 <img src={cover} />
                 <h1>{title}</h1>
                 <h3>{author}</h3>
                 <button className={styles["show-details"]}>Show details</button>
             </article>
+        // </div>
     )
 }
